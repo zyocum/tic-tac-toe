3ddef28c3e165a0eae1eba8d6df72b4853412731
Zachary Yocum <zachary.yocum@me.com>
2017-08-01 23:06:35 -0400

    add script

---
+++ tic_tac_toe.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+"""Run a game of Tic-Tac-Toe with curses"""
+
+import curses
+import numpy as np
+
+UP = 'K'
+DOWN = 'J'
+RIGHT = 'L'
+LEFT = 'H'
+
+X = 'X'
+O = 'O'
+
+CLEAR = 'C'
+DELETE = ('~', '\x7f')
+QUIT = 'Q'
+
+BOARD = (
+    '┌─┬─┬─┐\n'
+    '│{}│{}│{}│\n'
+    '├─┼─┼─┤\n'
+    '│{}│{}│{}│\n'
+    '├─┼─┼─┤\n'
+    '│{}│{}│{}│\n'
+    '└─┴─┴─┘\n'
+)
+
+def new_game():
+    return np.array([[None] * 3] * 3)
+
+def winner(game):
+    """Determine if there is a winner and if so return the winning symbol"""
+    for player in X, O:
+        if any((
+            # check diagonals
+            set(game.diagonal()) == {player},
+            set(np.fliplr(game).diagonal()) == {player},
+            # check rows
+            {player} in map(set, game),
+            # check columns
+            {player} in map(set, game.T)
+        )):
+            return player
+
+def cell(y, x):
+    """Derive the appropriate game state cell from curses window y, x
+    coordinates by correcting for the board offsets
+    
+    y: y position in the curses window
+    x: x position in the curses window
+    """
+    return y // 2, x // 2
+
+def run(game, debug=False):
+    """Start a game in a curses window
+    
+    game: the current state as a 3x3 numpy array
+    """
+    if not isinstance(game, np.ndarray) or game.shape != (3, 3):
+        raise ValueError('invalid game state (must be a 3x3 numpy.array)')
+    # set up curses window
+    window = curses.initscr()
+    curses.noecho()
+    window.erase()
+    try:
+        y, x = 1, 1 # start cursor in the upper left cell
+        value = None
+        # loop until the user quits
+        while not value == QUIT:
+            board = BOARD.format(*((v or ' ') for v in game.flatten()))
+            window.addstr(0, 0, board)
+            message = (
+                f'{LEFT}:left {RIGHT}:right {UP}:up {DOWN}:down\n'
+                f'{X}:"X" {O}:"O" ⌫:delete {CLEAR}:clear\n'
+                f'{QUIT}:quit'
+            )
+            window.addstr(9, 0, message)
+            value, *_ = window.getkey(y, x).upper()
+            # check for relevant input
+            if value in UP:
+                y = max(1, y - 2)
+            elif value in DOWN:
+                y = min(5, y + 2)
+            elif value in RIGHT:
+                x = min(5, x + 2)
+            elif value in LEFT:
+                x = max(1, x - 2)
+            elif value in X:
+                game[cell(y, x)] = X
+            elif value in O:
+                game[cell(y, x)] = O
+            elif value in DELETE:
+                game[cell(y, x)] = None
+            elif value in CLEAR:
+                game = new_game()
+            else:
+                curses.flash()
+            player = winner(game)
+            if player:
+                window.addstr(7, 0, f'{player} wins!')
+            elif np.all(game != None): # draw game
+                window.addstr(7, 0, "Draw!  ")
+            else:
+                window.addstr(7, 0, ' ' * 7)
+            if debug:
+                window.addstr(13, 0, f'value: {value} ')
+                window.addstr(14, 0, f'cursor: ({x}, {y})')
+                window.addstr(15, 0, f'game:\n{game}')
+    finally:
+        curses.endwin()
+
+if __name__ == '__main__':
+    run(new_game())
